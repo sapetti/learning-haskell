e9e7613627a33538b6253e083fe7792b3a45fe43
César Sapetti <gsapetti@hotmail.com>
2020-05-01 21:55:32 +0200

    Update chapter 4

--- chapter4.hs
+++ chapter4.hs
@@ -17,3 +17,31 @@ safetail1 xs = if null xs then [] else tail xs
 
 safetail2 xs | null xs   = []
              | otherwise = tail xs
+
+safetail3 [] = []
+safetail3 [x] = []
+safetail3 (x:xs) = xs
+
+(||) :: Bool -> Bool -> Bool
+True || _ = True
+_ || True = True
+False || False = False
+
+or :: Bool -> Bool -> Bool
+or a b = if a && b then True 
+         else False
+
+mult :: Int -> (Int -> (Int -> Int))
+mult = \x -> (\y -> (\z -> x*y*z))
+
+luhnDouble :: Int -> Int
+luhnDouble n | n * 2 > 9 = n * 2 -9
+             | otherwise = n * 2 
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn x y z v = result `mod` 10 > 0
+               where
+                  result = resultX + resultY + resultZ
+                  resultX = luhnDouble x
+                  resultY = luhnDouble y
+                  resultZ = luhnDouble z
